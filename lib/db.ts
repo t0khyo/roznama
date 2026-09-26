3cff24a4263334359fb7678f9d62c6a1643ff4db
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2026-09-26 20:11:08 +0300

    fix: update PrismaClient import to use edge version

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
